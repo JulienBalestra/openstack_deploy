b1591db6aa12d6d07186563be238a7b6c2d2fa8c
Julien Balestra <julien.balestra@gmail.com>
2016-01-15 18:24:35 +0100

    Use script to run test

---
+++ run_test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+function go_to_dirname
+{
+    echo "Go to working directory..."
+    cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+    if [ $? -ne 0 ]
+    then
+        echo "go_to_dirname failed";
+        exit 1
+    fi
+    echo "-> Current directory is" $(pwd)
+}
+
+function run_tests
+{
+    export LC_ALL=C
+    python -m unittest discover $(pwd)/tests/
+    if [ $? -ne 0 ]
+    then
+        exit 2
+    fi
+}
+
+function main
+{
+    go_to_dirname
+    run_tests
+}
+
+main
+exit $?
