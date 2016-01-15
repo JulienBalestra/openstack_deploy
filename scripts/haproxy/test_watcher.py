622f9a2635d1eb43cb754dae6a7159903132294c
Julien Balestra <julien.balestra@gmail.com>
2016-01-15 18:14:28 +0100

    HA Proxy Watcher

---
+++ scripts/haproxy/test_watcher.py
@@ -0,0 +1,106 @@
+import unittest
+import os
+
+import json
+from mock import patch, Mock
+
+import watcher
+
+
+class TestWatcherModify(unittest.TestCase):
+	w = type(watcher.Watcher)
+	hajson_path = "etc_haproxy_servers.json"
+	hacfg_path = "etc_haproxy_haproxy.cfg"
+	habase_path = "etc_haproxy_haproxy_base.cfg"
+
+	@classmethod
+	def setUpClass(cls):
+		os.chdir("%s/test_resources" % os.path.dirname(__file__))
+		cls.w = watcher.Watcher(
+				meta_url="mock",
+				hajson_path=cls.hajson_path,
+				hacfg_path=cls.hacfg_path,
+				habase_path=cls.habase_path,
+				port=80
+		)
+
+	@classmethod
+	def tearDownClass(cls):
+		with open("etc_haproxy_servers.json", 'w') as f:
+			f.write("[]")
+
+	def test_00_get_metadata(self):
+		@patch('watcher.urllib2.urlopen')
+		def mock_metadata(mock_urlopen):
+			a = Mock()
+			a.read.side_effect = [json.dumps({"servers": ["192.168.0.1"]})]
+			mock_urlopen.return_value = a
+			res = self.w._get_metadata()
+			assert res == [u'192.168.0.1']
+
+		mock_metadata()
+		self.assertEqual([u'192.168.0.1'], self.w.metadata_servers)
+
+	def test_01_get_current_server_list(self):
+		self.w._get_current_server_list()
+		self.assertEqual([], self.w.haproxy_servers)
+
+	def test_02_need_update(self):
+		self.assertTrue(self.w._need_update())
+
+	def test_03_update_haproxy_json(self):
+		with open(self.w.hajson_path, 'r') as f:
+			self.assertEqual('[]', f.read())
+		self.w._update_haproxy_json()
+		with open(self.w.hajson_path, 'r') as f:
+			self.assertEqual('["192.168.0.1"]', f.read())
+
+	def test_04_generate_new_haproxy_cfg(self):
+		self.w._generate_new_haproxy_cfg()
+		with open(self.hacfg_path, 'r') as cfg:
+			content = cfg.read()
+		self.assertIn("    server server-0 192.168.0.1:80", content)
+
+
+class TestWatcherunModify(unittest.TestCase):
+	w = type(watcher.Watcher)
+	hajson_path = "etc_haproxy_servers.json"
+	hacfg_path = "etc_haproxy_haproxy.cfg"
+	habase_path = "etc_haproxy_haproxy_base.cfg"
+
+	@classmethod
+	def setUpClass(cls):
+		os.chdir("%s/test_resources" % os.path.dirname(__file__))
+		cls.w = watcher.Watcher(
+				meta_url="mock",
+				hajson_path=cls.hajson_path,
+				hacfg_path=cls.hacfg_path,
+				habase_path=cls.habase_path,
+				port=80
+		)
+		with open("etc_haproxy_servers.json", 'w') as f:
+			f.write('["192.168.0.1"]')
+
+	@classmethod
+	def tearDownClass(cls):
+		with open("etc_haproxy_servers.json", 'w') as f:
+			f.write("[]")
+
+	def test_00_get_metadata(self):
+		@patch('watcher.urllib2.urlopen')
+		def mock_metadata(mock_urlopen):
+			a = Mock()
+			a.read.side_effect = [json.dumps({"servers": ["192.168.0.1"]})]
+			mock_urlopen.return_value = a
+			res = self.w._get_metadata()
+			assert res == [u'192.168.0.1']
+
+		mock_metadata()
+		self.assertEqual([u'192.168.0.1'], self.w.metadata_servers)
+
+	def test_01_get_current_server_list(self):
+		self.w._get_current_server_list()
+		self.assertEqual([u'192.168.0.1'], self.w.haproxy_servers)
+
+	def test_02_need_update(self):
+		self.assertFalse(self.w._need_update())
---
+++ scripts/haproxy/watcher.py
@@ -0,0 +1,90 @@
+import json
+import subprocess
+import argparse
+import urllib2
+
+
+class Watcher(object):
+	def __init__(self, meta_url, hajson_path, hacfg_path, habase_path, port):
+		self.meta_url = meta_url
+		self.metadata_servers = list()
+		self.hajson_path = hajson_path
+		self.haproxy_servers = list()
+		self.hacfg_path = hacfg_path
+		self.habase_path = habase_path
+		self.port = port
+
+	def _get_metadata(self):
+		req = urllib2.Request(self.meta_url)
+		res = urllib2.urlopen(req)
+		metadata = json.loads(res.read())
+		res.close()
+		try:
+			self.metadata_servers = metadata['servers']
+			return self.metadata_servers
+		except KeyError:
+			raise KeyError("cannot fetch server in metadata\n%s\n%s" % (self.metadata_servers, metadata))
+
+	def _get_current_server_list(self):
+		with open(self.hajson_path, 'r') as curr:
+			self.haproxy_servers = json.loads(curr.read())
+
+	def _need_update(self):
+		self.metadata_servers.sort()
+		self.haproxy_servers.sort()
+		if self.metadata_servers == self.haproxy_servers:
+			return False
+		return True
+
+	def _update_haproxy_json(self):
+		with open(self.hajson_path, "wt") as jcfg:
+			jcfg.write(json.dumps(self.metadata_servers))
+
+	def _generate_new_haproxy_cfg(self):
+		with open(self.habase_path, "r") as ref:
+			reference = ref.read()
+
+		with open(self.hacfg_path, "wt") as ha_cfg:
+			ha_cfg.write(reference)
+			ha_cfg.write("""
+listen app *:80
+	mode http
+	balance roundrobin
+	option httpclose
+	option forwardfor\n""")
+			for i, ip in enumerate(self.metadata_servers):
+				ha_cfg.write('    server server-{0} {1}:{2}\n'.format(i, ip, self.port))
+
+	def _reload_service(self):
+		cmd = ['/usr/sbin/service', 'haproxy', 'reload']
+		ret = subprocess.call(cmd)
+		if ret != 0:
+			raise SystemError("cant reload config %s" % " ".join(cmd))
+
+	def do(self):
+		self._get_metadata()
+		self._get_current_server_list()
+		if self._need_update():
+			self._update_haproxy_json()
+			self._generate_new_haproxy_cfg()
+			self._reload_service()
+
+
+if __name__ == "__main__":
+	args = argparse.ArgumentParser()
+	args.add_argument('-p', '--port', type=int, default=80, help='Destination port')
+	args.add_argument('-m', '--meta_url', type=str, default="http://169.254.169.254/openstack/latest/meta_data.json",
+					  help='URL for metadata')
+	args.add_argument('-j', '--haproxy_json', type=str, default="/etc/haproxy/servers.json",
+					  help='HA Proxy Json list of servers')
+	args.add_argument('-c', '--haproxy_config', type=str, default="/etc/haproxy/haproxy.cfg",
+					  help='HA Proxy config file')
+	args.add_argument('-b', '--haproxy_base', type=str, default="/etc/haproxy/haproxy_base.cfg",
+					  help='HA Proxy base for config file')
+	ap = args.parse_args()
+	w = Watcher(meta_url=ap.meta_url,
+				hajson_path=ap.haproxy_json,
+				hacfg_path=ap.haproxy_config,
+				habase_path=ap.haref_base,
+				port=ap.port)
+	w.do()
