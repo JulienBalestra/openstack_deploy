ee39e591a6c8c8995a0531f2d9a184b1b5ae5015
Julien Balestra <julien.balestra@gmail.com>
2016-02-06 14:21:38 +0100

    Testing suite

--- scripts/heat_tools/tests/test_check_stack_integrity.py
+++ scripts/heat_tools/tests/test_check_stack_integrity.py
@@ -4,8 +4,8 @@ import os
 from scripts.heat_tools import check_stack_integrity
 
 
-class TestCheckStackIntegrity(unittest.TestCase):
-	file_path = "test_resources/etcd.yaml"
+class TestCheckStackIntegrityValid(unittest.TestCase):
+	file_path = "test_resources/etcd_valid.yaml"
 	csi = check_stack_integrity.CheckStackIntegrity
 
 	@classmethod
@@ -15,3 +15,17 @@ class TestCheckStackIntegrity(unittest.TestCase):
 
 	def test_parameters(self):
 		self.csi.assert_params_equality()
+
+
+class TestCheckStackIntegrityInvalid(unittest.TestCase):
+	file_path = "test_resources/etcd_invalid.yaml"
+	csi = check_stack_integrity.CheckStackIntegrity
+
+	@classmethod
+	def setUpClass(cls):
+		os.chdir(os.path.dirname(__file__))
+		cls.csi = check_stack_integrity.CheckStackIntegrity(cls.file_path)
+
+	def test_parameters(self):
+		with self.assertRaises(AssertionError):
+			self.csi.assert_params_equality()
