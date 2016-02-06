e7c09adb4009b7d2aaede9bdfaab3f82c77afcd9
Julien Balestra <julien.balestra@gmail.com>
2016-02-06 19:14:52 +0100

    Capable to check ouputs

--- scripts/heat_tools/stack_integrity.py
+++ scripts/heat_tools/stack_integrity.py
@@ -1,7 +1,7 @@
 import yaml
 
 
-class CheckStackIntegrity(object):
+class StackIntegrity(object):
 	def __init__(self, file_path):
 		self.content = self._get_content(file_path)
 		self.parameters = self.content["parameters"]
@@ -9,27 +9,31 @@ class CheckStackIntegrity(object):
 		self.heat_template_version = self.content["heat_template_version"]
 		self.description = self.content["description"]
 		self.resources = self.content["resources"]
+
 		self.used_params = set()
 		self.declared_params = set()
+
+		self.used_outputs = set()
+		self.declared_outputs = set()
 
 	@staticmethod
 	def _get_content(file_path):
 		with open(file_path, 'r') as fd:
 			return yaml.load(fd)
 
-	def _find_orphans_params(self):
-		print self.parameters
-
-	def _set_used_type(self, get_type, section):
+	def _set_used_type(self, get_type, section, set_to_add):
 		if type(section) is list:
 			for i in section:
-				CheckStackIntegrity._set_used_type(self, get_type, i)
+				self._set_used_type(get_type, i, set_to_add)
 		elif type(section) is dict:
 			for key, value in section.iteritems():
 				if key == get_type:
-					self.used_params.add(value)
+					try:
+						set_to_add.add(value)
+					except TypeError:
+						set_to_add.add(value[0])
 				else:
-					CheckStackIntegrity._set_used_type(self, get_type, value)
+					self._set_used_type(get_type, value, set_to_add)
 
 	def _set_parameters_section(self):
 		for i in self.parameters:
@@ -37,10 +41,10 @@ class CheckStackIntegrity(object):
 
 	def assert_params_equality(self):
 		self._set_parameters_section()
-		self._set_used_type('get_param', self.resources)
+		self._set_used_type('get_param', self.resources, self.used_params)
 		assert self.declared_params == self.used_params
 
-
-if __name__ == "__main__":
-	p = CheckStackIntegrity("etcd.yaml")
-	p.assert_params_equality()
+	def capable_outputs(self):
+		self._set_used_type('get_attr', self.outputs, self.declared_outputs)
+		resources_names = set(self.resources.keys())
+		assert len(self.declared_outputs - resources_names) == 0
--- scripts/heat_tools/tests/test_stack_integrity.py
+++ scripts/heat_tools/tests/test_stack_integrity.py
@@ -1,31 +1,38 @@
 import unittest
 import os
 
-from scripts.heat_tools import check_stack_integrity
+from scripts.heat_tools import stack_integrity
 
 
 class TestCheckStackIntegrityValid(unittest.TestCase):
 	file_path = "test_resources/etcd_valid.yaml"
-	csi = check_stack_integrity.CheckStackIntegrity
+	csi = stack_integrity.StackIntegrity
 
 	@classmethod
 	def setUpClass(cls):
 		os.chdir(os.path.dirname(__file__))
-		cls.csi = check_stack_integrity.CheckStackIntegrity(cls.file_path)
+		cls.csi = stack_integrity.StackIntegrity(cls.file_path)
 
 	def test_parameters(self):
 		self.csi.assert_params_equality()
 
+	def test_outputs(self):
+		self.csi.capable_outputs()
+
 
 class TestCheckStackIntegrityInvalid(unittest.TestCase):
 	file_path = "test_resources/etcd_invalid.yaml"
-	csi = check_stack_integrity.CheckStackIntegrity
+	csi = stack_integrity.StackIntegrity
 
 	@classmethod
 	def setUpClass(cls):
 		os.chdir(os.path.dirname(__file__))
-		cls.csi = check_stack_integrity.CheckStackIntegrity(cls.file_path)
+		cls.csi = stack_integrity.StackIntegrity(cls.file_path)
 
 	def test_parameters(self):
 		with self.assertRaises(AssertionError):
 			self.csi.assert_params_equality()
+
+	def test_outputs(self):
+		with self.assertRaises(AssertionError):
+			self.csi.capable_outputs()
