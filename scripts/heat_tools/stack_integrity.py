68ea779e6c34717e236c0cf79f1974e5cbf92504
Julien Balestra <julien.balestra@gmail.com>
2016-02-09 14:08:55 +0100

    Verbose mode

--- scripts/heat_tools/stack_integrity.py
+++ scripts/heat_tools/stack_integrity.py
@@ -10,25 +10,26 @@ class StackIntegrity(object):
 	appends = ["depends_on"]
 	registry_file_directory = None
 
-	def __init__(self, stack_file_path, registry_file_path, parameters=None):
+	def __init__(self, stack_file_path, registry_file_path, parameters=None, verbose=False):
+		self.verbose = verbose
+		if verbose:
+			print "Instance for %s" % stack_file_path
 		if parameters:
 			self.given_parameters = set([k for k in parameters])
 
 		self.registry_file_path = registry_file_path
-		if StackIntegrity.registry_file_directory is None:
+		if StackIntegrity.registry_file_directory is None and registry_file_path is not None:
 			StackIntegrity.registry_file_directory = os.path.split(os.path.abspath(registry_file_path))[0]
-			print StackIntegrity.registry_file_directory
 
-		self.stack_content = self._get_content(stack_file_path)
+		stack_content = self._get_content(stack_file_path)
+		self.parameters = stack_content["parameters"]
+		self.outputs = stack_content["outputs"] if stack_content.has_key("outputs") else dict()
+		self.heat_template_version = stack_content["heat_template_version"]
+		self.description = stack_content["description"]
+		self.resources = stack_content["resources"]
 
-		self.parameters = self.stack_content["parameters"]
-		if self.stack_content.has_key("outputs"):
-			self.outputs = self.stack_content["outputs"]
-		else:
-			self.outputs = dict()
-		self.heat_template_version = self.stack_content["heat_template_version"]
-		self.description = self.stack_content["description"]
-		self.resources = self.stack_content["resources"]
+		registry_content = self._get_content(registry_file_path) if registry_file_path else None
+		self.registries = registry_content["resource_registry"] if registry_file_path else {}
 
 		self.used_params = set()
 		self.declared_params = set()
@@ -36,10 +37,6 @@ class StackIntegrity(object):
 		self.declared_outputs = set()
 		self.used_resources = set()
 		self.declared_resources = set()
-
-		self.registry_content = self._get_content(registry_file_path)
-
-		self.registries = self.registry_content["resource_registry"]
 		self.used_registries = set()
 
 	def _get_content(self, file_path):
@@ -71,8 +68,9 @@ class StackIntegrity(object):
 		if type(section) is dict:
 			for key, value in section.iteritems():
 				if key == "type" and value in self.registries:
-					nested_file_path = self.registry_content['resource_registry'][value]
-					r = StackIntegrity(nested_file_path, self.registry_file_path, section["properties"].keys())
+					nested_file_path = self.registries[value]
+					r = StackIntegrity(
+							nested_file_path, self.registry_file_path, section["properties"].keys(), self.verbose)
 					r.do_all()
 				else:
 					self._deep_inside_nested_stack(get_type, value)
@@ -113,24 +111,34 @@ class StackIntegrity(object):
 
 	def right_depends(self):
 		depends = set()
+		resources_keys_set = set(self.resources.keys())
 		self._set_used_type("depends_on", self.resources, depends)
-		if len(depends - set(self.resources.keys())) != 0:
-			raise AssertionError(
-					"%d != 0 %s" % (len(depends - set(self.resources.keys())),
-									depends - set(self.resources.keys())))
+		if len(depends - resources_keys_set) != 0:
+			raise AssertionError("%d != 0 %s" % (len(depends - resources_keys_set), depends - resources_keys_set))
+
+	@staticmethod
+	def runner(func, verbose):
+		if verbose:
+			print func.__name__, "..."
+		func()
+		if verbose:
+			print func.__name__, "OK\n"
 
 	def do_all(self):
-		self.right_depends()
-		self.capable_resources()
-		self.capable_outputs()
-		self.assert_params_equality()
-		self.capable_parameters()
-		self.follow_nested_stack()
+		for f in [
+			self.right_depends,
+			self.capable_resources,
+			self.capable_outputs,
+			self.assert_params_equality,
+			self.capable_parameters,
+			self.follow_nested_stack
+		]:
+			self.runner(f, self.verbose)
 
 	def follow_nested_stack(self):
 		types = set()
 		self._set_used_type("type", self.resources, types)
-		self.used_registries = types.intersection(self.registries.keys())
+		self.used_registries = types.intersection()
 		for i in self.registries:
 			self._deep_inside_nested_stack(i, self.resources)
 
@@ -140,14 +148,25 @@ def fast_arg_parsing():
 	args.add_argument("-f", "--stack_file", type=str, help="stack_file")
 	args.add_argument("-P", "--parameter", nargs="*", type=str, help="key=value")
 	args.add_argument("-r", "--registry", type=str, help="registry_file")
+	args.add_argument("-v", "--verbose", type=bool, default=False, help="verbose")
 
-	return args.parse_args().stack_file, args.parse_args().parameter, args.parse_args().registry
+	return args.parse_args().stack_file, args.parse_args().parameter, args.parse_args().registry, \
+		   args.parse_args().verbose
 
 
 if __name__ == "__main__":
-	stack_file, parameters, registry_file_directory = fast_arg_parsing()
+	stack_file, parameters, registry_file_directory, verbose = fast_arg_parsing()
+	if verbose:
+		print "stack_file %s\n" \
+			  "parameters %s\n" \
+			  "registry_file_directory %s\n" % (stack_file, parameters, registry_file_directory)
+
+	if stack_file is None:
+		raise AttributeError
 	integrity = StackIntegrity(
 			stack_file_path=stack_file,
 			registry_file_path=registry_file_directory,
-			parameters=parameters)
+			parameters=parameters,
+			verbose=verbose
+	)
 	integrity.do_all()
