eb1a97610d3f89b2f08a78e685d8a01eee6acb8a
Julien Balestra <julien.balestra@gmail.com>
2016-02-10 16:15:02 +0100

    Ci for fast instance

---
+++ heat/fast_instance/integrity.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+function go_to_dirname
+{
+    echo "Go to working directory..."
+    cd $( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd -L && pwd -P )
+    if [ $? -ne 0 ]
+    then
+        echo "go_to_dirname failed";
+        exit 1
+    fi
+    echo "-> Current directory is" $(pwd)
+}
+
+go_to_dirname
+
+
+../../scripts/heat_tools/stack_integrity.py \
+-v True \
+-f fast.yaml \
+-r registry.yaml \
+-P  "context" \
+    "image" \
+    "flavor" \
+    "dns_nameservers" \
+    "key_name" \
+    "floatingip_network_name"
